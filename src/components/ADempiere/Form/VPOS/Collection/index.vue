927a4d6085a35ab8eb3d29162b6a5bdc66157370
Raúl Capecce <rcapecce@gmail.com>
2021-12-27 19:09:05 -0300

    Disabling checkout button when order is processed (#1456)

--- src/components/ADempiere/Form/VPOS/Collection/index.vue
+++ src/components/ADempiere/Form/VPOS/Collection/index.vue
@@ -103,7 +103,7 @@
             <el-button type="danger" icon="el-icon-close" @click="exit" />
             <el-button type="info" icon="el-icon-minus" :disabled="isDisabled" @click="undoPatment" />
             <el-button type="success" icon="el-icon-plus" :disabled="validPay || addPay || isDisabled" @click="addCollectToList(paymentBox)" />
-            <el-button type="primary" :disabled="validatePaymentBeforeProcessing" icon="el-icon-shopping-cart-full" @click="validateOrder(listPayments)" />
+            <el-button type="primary" :disabled="validatePaymentBeforeProcessing || porcessInvoce" icon="el-icon-shopping-cart-full" @click="validateOrder(listPayments)" />
           </samp>
         </el-header>
         <!-- Panel where they show the payments registered from the collection container -->
