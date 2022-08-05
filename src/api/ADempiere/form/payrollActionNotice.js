76376a16b2b0cda2ab4d558838020d72a0f47bc1
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2022-08-05 18:25:54 -0400

    Add service payroll Action Notice (#312)

---
+++ src/api/ADempiere/form/payrollActionNotice.js
@@ -0,0 +1,126 @@
+// ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+// Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A.
+// Contributor(s): Yamel Senih ysenih@erpya.com www.erpya.com
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import { request } from '@/utils/ADempiere/request'
+import { config } from '@/utils/ADempiere/config'
+
+// List Scale
+export function listPayrollProcess() {
+  return request({
+    url: `${config.payrollActionNotice.endpoint}/list-payroll-process`,
+    method: 'get'
+  })
+    .then(response => {
+      return response
+    })
+}
+
+export function listEmployeeValid({
+  contextAttributes
+}) {
+  return request({
+    url: `${config.payrollActionNotice.endpoint}/list-employee-valid`,
+    method: 'get',
+    params: {
+      context_attributes: contextAttributes
+    }
+  })
+    .then(response => {
+      return response
+    })
+}
+
+export function listPayrollConcepts({
+  contextAttributes,
+  searchValue
+}) {
+  return request({
+    url: `${config.payrollActionNotice.endpoint}/list-payroll-concepts`,
+    method: 'get',
+    params: {
+      context_attributes: contextAttributes,
+      search_value: searchValue
+    }
+  })
+    .then(response => {
+      return response
+    })
+}
+
+export function listPayrollMovements({
+  contextAttributes,
+  filters,
+  searchValue
+}) {
+  return request({
+    url: `${config.payrollActionNotice.endpoint}/list-payroll-movements`,
+    method: 'get',
+    params: {
+      filters,
+      context_attributes: contextAttributes,
+      search_value: searchValue
+    }
+  })
+    .then(response => {
+      return response
+    })
+}
+
+export function listPayrollMovement({
+  id,
+  uuid,
+  contextAttributes,
+  filters,
+  attributes
+}) {
+  return request({
+    url: `${config.payrollActionNotice.endpoint}/list-payroll-movement`,
+    method: 'get',
+    params: {
+      filters,
+      id,
+      uuid,
+      context_attributes: contextAttributes,
+      attributes
+    }
+  })
+    .then(response => {
+      return response
+    })
+}
+
+export function deletePayrollMovement({
+  id,
+  uuids,
+  contextAttributes,
+  filters,
+  attributes
+}) {
+  return request({
+    url: `${config.payrollActionNotice.endpoint}/delete-payroll-movement`,
+    method: 'get',
+    params: {
+      filters,
+      id,
+      uuids,
+      context_attributes: contextAttributes,
+      attributes
+    }
+  })
+    .then(response => {
+      return response
+    })
+}
