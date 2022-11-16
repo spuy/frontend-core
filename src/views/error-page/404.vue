14351866e93b30c7e68f5904ea9c1c1f05802e4e
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-11-15 22:37:46 -0400

    fix: 404 Page focus back button. (#578)

--- src/views/error-page/404.vue
+++ src/views/error-page/404.vue
@@ -14,7 +14,10 @@
         </div>
         <div class="bullshit__headline">{{ message }}</div>
         <div class="bullshit__info">Please check that the URL you entered is correct, or click the button below to return to the homepage.</div>
-        <a href="" class="bullshit__return-home">Back to home</a>
+
+        <a ref="backToHome" href="" class="bullshit__return-home">
+          Back to home
+        </a>
       </div>
     </div>
   </div>
@@ -28,6 +31,9 @@ export default {
     message() {
       return 'The webmaster said that you can not enter this page...'
     }
+  },
+  mounted() {
+    this.$refs['backToHome'].focus()
   }
 }
 </script>
