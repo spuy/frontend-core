30e3d9b0bb4d3cdbc47a1bc25b127fce92e0a3cb
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2022-12-22 19:13:18 -0400

    Fix: Support Exists References (#667)
    * Fix: Support Exists Referenc
    
    * Update window.js
    
    Co-authored-by: Edwin Betancourt <EdwinBetanc0urt@outlook.com>

--- src/api/ADempiere/window.js
+++ src/api/ADempiere/window.js
@@ -1,23 +1,24 @@
-// ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
-// Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A.
-// Contributor(s): Yamel Senih ysenih@erpya.com www.erpya.com
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/**
+ * ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+ * Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 // Get Instance for connection
 import { request } from '@/utils/ADempiere/request'
 import { isEmptyValue } from '@/utils/ADempiere/valueUtils.js'
-
 /**
  * Request a Lookup list data from Reference
  * The main attributes that function hope are:
@@ -117,6 +118,34 @@ export function requestReferencesList({
       const { convertReferencesList } = require('@/utils/ADempiere/apiConverts/values.js')
 
       return convertReferencesList(referencesListResposnse)
+    })
+}
+
+/**
+ * Reference List from Window
+ * @param {number}  tabId
+ * @param {string}  tabUuid
+ * @param {number}  recordId
+ * @param {string}  recordUuid
+ */
+export function requestExistsReferences({
+  tabId,
+  tabUuid,
+  recordId,
+  recordUuid
+}) {
+  return request({
+    url: '/user-interface/window/exists-references',
+    method: 'get',
+    params: {
+      record_id: recordId,
+      record_uuid: recordUuid,
+      tab_id: tabId,
+      tab_uuid: tabUuid
+    }
+  })
+    .then(referencesListResposnse => {
+      return referencesListResposnse
     })
 }
 
