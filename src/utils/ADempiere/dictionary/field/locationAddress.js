276364fd0c4284f534c36ac8007dee1544cdde96
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2023-03-13 13:53:21 -0400

    fix: Field Location Address with mandatory on capture sequence. (#925)

--- src/utils/ADempiere/dictionary/field/locationAddress.js
+++ src/utils/ADempiere/dictionary/field/locationAddress.js
@@ -23,6 +23,34 @@ export const COLUMN_NAME = 'C_Location_ID'
 export const LOCATION_ADDRESS_FORM = 'Location-Address'
 
 /**
+ * Field Column Names
+ */
+export const COLUMNNAME_City = 'City'
+
+export const COLUMNNAME_C_City_ID = 'C_City_ID'
+
+export const COLUMNNAME_Postal = 'Postal'
+
+export const COLUMNNAME_Postal_Add = 'Postal_Add'
+
+export const COLUMNNAME_RegionName = 'RegionName'
+
+export const COLUMNNAME_C_Region_ID = 'C_Region_ID'
+
+export const COLUMNNAME_C_Country_ID = 'C_Country_ID'
+
+export const COLUMNNAME_Altitude = 'Altitude'
+
+export const COLUMNNAME_Latitude = 'Latitude'
+
+export const COLUMNNAME_Longitude = 'Longitude'
+
+/**
+ * Is Mandatory field on sequence
+ */
+export const MANDATORY_CHAR = '!'
+
+/**
  * Use this function for get a list of sequence of capture for locations
  * TODO: Evaluate capture sequence by Germany "@A1@ @A2@ @A3@ @A4@ D-@P@ @R@ @C@ @CO@" with "D-" suffix in postal code
  * @param {string} captureSequence
@@ -32,21 +60,36 @@ export function getSequenceAsList(captureSequence) {
   if (!captureSequence) {
     return undefined
   }
-  //  Split it
-  return captureSequence
+  // Split it
+  const sequenceFields = captureSequence
     .replace('@@', '@')
     .replace(',', '')
     .trim()
     .split('@')
     .filter(value => value.trim())
+
+  const sequenceWithMandatory = []
+  sequenceFields.forEach(sequence => {
+    const baseSequence = sequence.replace(MANDATORY_CHAR, '')
+    sequenceWithMandatory.push(baseSequence)
+
+    if (sequence.endsWith(MANDATORY_CHAR)) {
+      // sequence with mandatory
+      sequenceWithMandatory.push(
+        baseSequence + MANDATORY_CHAR
+      )
+    }
+  })
+
+  return sequenceWithMandatory
 }
 
 export const URL_BASE_MAP = 'https://www.google.com/maps?q='
 
 export const COORDENATES_COLUMN_NAMES = [
-  'Latitude',
-  'Longitude',
-  'Altitude'
+  COLUMNNAME_Altitude,
+  COLUMNNAME_Latitude,
+  COLUMNNAME_Longitude
 ]
 
 /**
