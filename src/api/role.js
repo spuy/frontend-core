aca8b0cab6c7f2340f4f8321650e0b4dabd3e163
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2023-03-22 10:31:37 -0400

    fix: Session UUID with change role / change organization. (#936)

--- src/api/role.js
+++ src/api/role.js
@@ -1,3 +1,21 @@
+/**
+ * ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+ * Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { request } from '@/utils/ADempiere/request'
 
 export function getRoutes() {
@@ -62,6 +80,7 @@ export function deleteRole(id) {
  * @param {string} warehouseUuid
  */
 export function requestChangeRole({
+  sessionUuid,
   roleUuid,
   organizationUuid,
   warehouseUuid
@@ -70,6 +89,7 @@ export function requestChangeRole({
     url: 'user/change-role',
     method: 'post',
     data: {
+      session_uuid: sessionUuid,
       role: roleUuid,
       organization: organizationUuid,
       warehouse: warehouseUuid
